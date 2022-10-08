872adc283a8a4dbf3ff2550ab1ca445cf5fe0215
Ayantika6767 <77482223+Ayantika6767@users.noreply.github.com>
2022-10-08 21:37:16 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,37 @@
+var bt1= document.getElementById('but1').onclick=function()
+{
+    alert('Order Added to Cart!');
+}
+const bt2= document.getElementById('but2').onclick=function()
+{
+    alert('Order Added to Cart!');
+}
+
+const bt3= document.getElementById('but3').onclick=function()
+{
+    alert('Order Added to Cart!');
+}
+
+const bt24= document.getElementById('but4').onclick=function()
+{
+    alert('Order Added to Cart!');
+}
+
+const bt5= document.getElementById('but5').onclick=function()
+{
+    alert('Order Added to Cart!');
+}
+const bt6= document.getElementById('but6').onclick=function()
+{
+    alert('Order Added to Cart!');
+}
+
+document.getElementById('locate-div').onclick=function()
+{
+    alert('Lets Take to Cafe Grub-Up!');
+}
+document.getElementById('locate').onclick=function()
+{
+    alert('Lets Take to Cafe Grub-Up!');
+}
+
